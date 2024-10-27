44f42d22500e6fdd625907507ebc40b1467ed399
Md-Siam-Mia-Code <mdsiammia.main@gmail.com>
2024-10-28 00:28:52 +0600

    Update GFPGAN.bat

--- GFPGAN.bat
+++ GFPGAN.bat
@@ -4,7 +4,7 @@
 CALL "C:\ProgramData\miniconda3\Scripts\activate.bat" GFPGAN
 
 :: Navigate to the GFPGAN directory (Change path accroding to yourself)
-cd /D G:\GFPGAN-Web-Interface
+cd /D path/to/your/GFPGAN-Web-Interface
 
 :: Run the GFPGAN web interface script
 python GFPGAN.py
